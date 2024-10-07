9eba240d6f10e12f15aab63c0e3007b4b4f46ed5
miyabin66 <makoto.furuzono@prtimes.co.jp>
2024-10-07 15:49:31 +0900

    判定の正常系のテスト作成

--- src/closedInterval.ts
+++ src/closedInterval.ts
@@ -1,12 +1,13 @@
 export class ClosedInterval {
   _startNum: number;
   _endNum: number;
+  _checkNum?: number;
 
-  constructor (startNum: number, endNum: number) {
+  constructor (startNum: number, endNum: number, checkNum?: number) {
     if (!Number.isInteger(startNum)) {
       throw new Error("第1引数に整数を入れてください");
     }
-    
+
     if (!Number.isInteger(endNum)) {
       throw new Error("第2引数に整数を入れてください");
     }
@@ -17,9 +18,16 @@ export class ClosedInterval {
 
     this._startNum = startNum;
     this._endNum = endNum;
+    this._checkNum = checkNum;
   }
 
   public get value() {
     return `[${this._startNum},${this._endNum}]`
+  }
+
+  public get isInsideValue() {
+    if (this._checkNum === undefined) return false;
+
+    return this._startNum <= this._checkNum && this._checkNum <= this._endNum
   }
 }
---
+++ test/整数の閉区間/3つの整数を渡すと指定した整数を含むかどうかを判定できる.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest';
+import { ClosedInterval } from '../../src/closedInterval'
+
+describe('3つの整数を渡すと指定した整数を含むかどうかを判定できる', () => {
+  test('1, 7, 3を渡すとtrueを返す', () => {
+    const closedInterval = new ClosedInterval(1, 7, 3);
+    expect(closedInterval.isInsideValue).toBe(true);
+  })
+
+  test('-1, 1, 0を渡すとtrueを返す', () => {
+    const closedInterval = new ClosedInterval(-1, 1, 0);
+    expect(closedInterval.isInsideValue).toBe(true);
+  })
+
+  test('1, 7, 10を渡すとfalseを返す', () => {
+    const closedInterval = new ClosedInterval(1, 7, 10);
+    expect(closedInterval.isInsideValue).toBe(false);
+  })
+})
