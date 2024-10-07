5ee5853796277270ef77ef6a25b3462e03dc348e
miyabin66 <makoto.furuzono@prtimes.co.jp>
2024-10-07 15:25:52 +0900

    第1引数が第2引数よりも大きい場合はエラーを返す

--- src/closedInterval.ts
+++ src/closedInterval.ts
@@ -6,9 +6,15 @@ export class ClosedInterval {
     if (!Number.isInteger(startNum)) {
       throw new Error("第1引数に整数を入れてください");
     }
+    
     if (!Number.isInteger(endNum)) {
       throw new Error("第2引数に整数を入れてください");
     }
+
+    if (startNum > endNum) {
+      throw new Error('第2引数より第1引数の方が大きいため閉区間が存在しません');
+    }
+
     this._startNum = startNum;
     this._endNum = endNum;
   }
