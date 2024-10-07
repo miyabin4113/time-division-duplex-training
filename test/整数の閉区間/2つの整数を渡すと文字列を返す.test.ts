4fca62f82948ad369dec4e911f366e37210b520b
miyabin66 <makoto.furuzono@prtimes.co.jp>
2024-10-07 15:13:50 +0900

    正しい挙動のテストの条件を追加

--- test/整数の閉区間/2つの整数を渡すと文字列を返す.test.ts
+++ test/整数の閉区間/2つの整数を渡すと文字列を返す.test.ts
@@ -1,9 +1,19 @@
 import { describe, expect, test } from 'vitest';
-import {ClosedInterval} from '../../src/closedInterval'
+import { ClosedInterval } from '../../src/closedInterval'
 
 describe('2つの整数を渡すと文字列を返す', () => {
   test('1, 7を渡すと `[1,7]` を返す', () => {
     const closedInterval = new ClosedInterval(1, 7);
     expect(closedInterval.value).toBe('[1,7]');
+  })
+
+  test('-1, 1を渡すと `[-1,1]` を返す', () => {
+    const closedInterval = new ClosedInterval(-1, 1);
+    expect(closedInterval.value).toBe('[-1,1]');
+  })
+
+  test('1, 1を渡すと `[1, 1]` を返す', () => {
+    const closedInterval = new ClosedInterval(1, 1);
+    expect(closedInterval.value).toBe('[1,1]');
   })
 })
